1a6119f6394bd555b7c01a5c5827ed1aa891cd47
B1naryB0b <35399675+B1naryB0b@users.noreply.github.com>
2024-03-22 03:54:09 +0000

    Revert Boid Manager + Fixed Audio Priority
    - reverted the boid manager due to strange boid behaviour
    - Adjusted audio source priority for certain enemy prefabs to make sure they were always heard

--- Assets/_Project/Scripts/Boids/BoidManager.cs
+++ Assets/_Project/Scripts/Boids/BoidManager.cs
@@ -1,104 +1,84 @@
-﻿using UnityEngine;
+﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
 
 public class BoidManager : MonoBehaviour
 {
-    const int ThreadGroupSize = 1024;
+
+    const int threadGroupSize = 1024;
 
     public BoidSettings settings;
     public ComputeShader compute;
     public Transform target;
 
     private List<Boid> boids = new List<Boid>();
-    private ComputeBuffer boidBuffer;
-    private BoidData[] boidData;
-
-    void Update()
-    {
-        int numBoids = boids.Count;
-        if (numBoids <= 0)
-        {
-            boids.RemoveAll(boid => boid == null);
-            return;
-        }
-
-        if (boidData == null || boidData.Length != numBoids)
-        {
-            if (boidBuffer != null)
-                boidBuffer.Release();
-
-            boidData = new BoidData[numBoids];
-            boidBuffer = new ComputeBuffer(numBoids, BoidData.Size);
-        }
-
-        for (int i = 0; i < numBoids; i++)
-        {
-            boidData[i].position = boids[i].position;
-            boidData[i].direction = boids[i].forward;
-        }
-
-        boidBuffer.SetData(boidData);
-
-        compute.SetBuffer(0, "boids", boidBuffer);
-        compute.SetInt("numBoids", numBoids);
-        compute.SetFloat("viewRadius", settings.perceptionRadius);
-        compute.SetFloat("avoidRadius", settings.avoidanceRadius);
-
-        int threadGroups = Mathf.CeilToInt(numBoids / (float)ThreadGroupSize);
-        compute.Dispatch(0, threadGroups, 1, 1);
-
-        boidBuffer.GetData(boidData);
-
-        for (int i = 0; i < numBoids; i++)
-        {
-            boids[i].avgFlockHeading = boidData[i].flockHeading;
-            boids[i].centreOfFlockmates = boidData[i].flockCentre;
-            boids[i].avgAvoidanceHeading = boidData[i].avoidanceHeading;
-            boids[i].numPerceivedFlockmates = boidData[i].numFlockmates;
-
-            boids[i].UpdateBoid();
-        }
-
-        boids.RemoveAll(boid => boid == null);
-    }
 
     public void AddBoid(Boid boid)
     {
         boids.Add(boid);
         boid.Initialize(settings, target);
-        UpdateBuffer();
     }
 
     public void RemoveBoid(Boid boid)
     {
         boids.Remove(boid);
-        UpdateBuffer();
     }
 
-    private void UpdateBuffer()
+    void Update()
     {
-        if (boidBuffer != null)
-            boidBuffer.Release();
+        if (boids.Count > 0)
+        {
+            int numBoids = boids.Count;
+            var boidData = new BoidData[numBoids];
 
-        boidData = new BoidData[boids.Count];
-        boidBuffer = new ComputeBuffer(boids.Count, BoidData.Size);
-    }
+            for (int i = 0; i < numBoids; i++)
+            {
+                boidData[i].position = boids[i].position;
+                boidData[i].direction = boids[i].forward;
+            }
 
-    void OnDestroy()
-    {
-        if (boidBuffer != null)
+            var boidBuffer = new ComputeBuffer(numBoids, BoidData.Size);
+            boidBuffer.SetData(boidData);
+
+            compute.SetBuffer(0, "boids", boidBuffer);
+            compute.SetInt("numBoids", numBoids);
+            compute.SetFloat("viewRadius", settings.perceptionRadius);
+            compute.SetFloat("avoidRadius", settings.avoidanceRadius);
+
+            int threadGroups = Mathf.CeilToInt(numBoids / (float)threadGroupSize);
+            compute.Dispatch(0, threadGroups, 1, 1);
+
+            boidBuffer.GetData(boidData);
+
+            for (int i = 0; i < numBoids; i++)
+            {
+                boids[i].avgFlockHeading = boidData[i].flockHeading;
+                boids[i].centreOfFlockmates = boidData[i].flockCentre;
+                boids[i].avgAvoidanceHeading = boidData[i].avoidanceHeading;
+                boids[i].numPerceivedFlockmates = boidData[i].numFlockmates;
+
+                boids[i].UpdateBoid();
+            }
+
             boidBuffer.Release();
+        }
+
+        boids.RemoveAll(boid => boid == null);
     }
 
     public struct BoidData
     {
         public Vector3 position;
         public Vector3 direction;
+
         public Vector3 flockHeading;
         public Vector3 flockCentre;
         public Vector3 avoidanceHeading;
         public int numFlockmates;
 
-        public static int Size => sizeof(float) * 3 * 5 + sizeof(int);
+        public static int Size
+        {
+            get { return sizeof(float) * 3 * 5 + sizeof(int); }
+        }
     }
 }
